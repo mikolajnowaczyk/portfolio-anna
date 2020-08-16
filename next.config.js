bfd316db14297c69693f7fcca30d4474a6688fc0
Mikolaj <mikolaj35@wp.pl>
2020-08-16 16:51:21 +0200

    Next.js images plugin
    Images configuration in next.config for images

--- next.config.js
+++ next.config.js
@@ -16,18 +16,66 @@ const assetPrefix = isProd ? '/portfolio-anna' : ''
 
 const withCSS = require('@zeit/next-css')
 
-module.exports = withCSS({
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+module.exports = withPlugins([
+  [optimizedImages, {
+    // these are the default values so you don't have to provide them if they are good enough for your use-case.
+    // but you can overwrite them here with any valid value you want.
+    inlineImageLimit: 8192,
+    imagesFolder: 'images',
+    imagesName: '[name]-[hash].[ext]',
+    handleImages: ['jpeg', 'png', 'svg', 'webp', 'gif'],
+    removeOriginalExtension: false,
+    optimizeImages: true,
+    optimizeImagesInDev: false,
+    mozjpeg: {
+      quality: 80,
+    },
+    optipng: {
+      optimizationLevel: 3,
+    },
+    pngquant: false,
+    gifsicle: {
+      interlaced: true,
+      optimizationLevel: 3,
+    },
+    svgo: {
+      // enable/disable svgo plugins here
+    },
+    webp: {
+      preset: 'default',
+      quality: 75,
+    },
+  }],
+  [withCSS, {
+    cssModules: true,
+    cssLoaderOptions: {
+      importLoaders: 1,
+      localIdentName: "[local]___[hash:base64:5]",
+    },
+  }],
+  {
+    assetPrefix: assetPrefix,
+    basePath: assetPrefix,
+    webpack: config => {
+      config.plugins.push(
+        new webpack.DefinePlugin({
+          'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix),
+        }),
+      )
+      return config
+    },
+  }
+])
+
+withCSS({
   cssModules: true,
   cssLoaderOptions: {
     importLoaders: 1,
     localIdentName: "[local]___[hash:base64:5]",
   },
-  exportPathMap: () => ({
-    '/': { page: '/' },
-    '/cennik': { page: '/cennik' },
-    '/kontakt': { page: '/kontakt' },
-    // '/page1': { page: '/page1' },
-  }),
   assetPrefix: assetPrefix,
   basePath: assetPrefix,
   webpack: config => {
@@ -36,7 +84,6 @@ module.exports = withCSS({
         'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix),
       }),
     )
-
     return config
   },
 })

2cf7d4eca7772ae525b5791ae93f3d865795ffb1
Mikolaj <mikolaj35@wp.pl>
2020-08-17 21:52:15 +0200

    Image path fix
    Image path fix

--- src/shared/images.js
+++ src/shared/images.js
@@ -1,13 +1,13 @@
-const test = "../images/test.jpg";
-const back = "../images/back.jpg";
-const back_hold = "../images/back_hold.jpg";
-const back_roll = "../images/back_roll.jpg";
-const hand = "../images/hand.jpg";
-const head_shoulder = "../images/head_shoulder.jpg";
-const massage = "../images/massage.jpg";
-const massage_oil = "../images/massage_oil.jpg";
-const massage_woman = "../images/massage_woman.jpg";
-const shoulder = "../images/shoulder.jpg";
+const test = "./images/test.jpg";
+const back = "./images/back.jpg";
+const back_hold = "./images/back_hold.jpg";
+const back_roll = "./images/back_roll.jpg";
+const hand = "./images/hand.jpg";
+const head_shoulder = "./images/head_shoulder.jpg";
+const massage = "./images/massage.jpg";
+const massage_oil = "./images/massage_oil.jpg";
+const massage_woman = "./images/massage_woman.jpg";
+const shoulder = "./images/shoulder.jpg";
 
 export {
   test,
